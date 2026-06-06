d3d705ac623a7bbf04492868f9b806014d460d43
Shakil Ahmed <shakil.ahmed.mobin@gmail.com>
2026-06-06 11:26:18 +0600

    initial commit

---
+++ 08_loops.py
@@ -0,0 +1,50 @@
+# i=0
+# n=int(input("enter how many time you want print: "))
+# while (i <= n):
+#     print("hello")
+#     i += 1
+
+
+# i=0
+# list=["Hello,","I'am Shakil.", "My number is:","01302912650"]
+# while (i < len(list)):
+#     print(list[i])
+#     i += 1
+
+
+# # Ques-1
+# i=0
+# x= 98
+# taple=(1,3,8,3,98,10,7)
+# while (i < len(taple)):
+#     if (taple[i]==x):
+#         print("found x at index ",i)
+#     else:
+#         print("not found")
+#     i += 1
+
+# # alternative:
+# i = 0
+# x = 98
+# taple = (1, 3, 8, 3, 98, 10, 7)
+# while i < len(taple):
+#     if taple[i] == x:
+#         print("found x at index", i)
+#         break
+#     i += 1
+# else:
+#     print("not found")
+
+
+# i=100
+# while i>=0:
+#     print(i)
+#     i-=1
+
+# i=1
+# while i<=100:
+#     print(i)
+#     i+=1
+
+
+
