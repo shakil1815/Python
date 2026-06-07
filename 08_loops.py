1d64a3700abc43b9ef0ca852018b0635371ef271
Shakil Ahmed <shakil.ahmed.mobin@gmail.com>
2026-06-08 01:15:17 +0600

    inital commit

--- 08_loops.py
+++ 08_loops.py
@@ -1,8 +1,8 @@
-# i=0
-# n=int(input("enter how many time you want print: "))
-# while (i <= n):
-#     print("hello")
-#     i += 1
+i=0
+n=int(input("enter how many time you want print: "))
+while (i <= n):
+    print("hello")
+    i += 1
 
 
 # i=0
@@ -47,4 +47,47 @@
 #     i+=1
 
 
+# i = 1
+# while i <= 100:
+#     if i % 2 == 0:
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+
+
+# # For loops :
+# taple = (1,66,88,2,3,)
+# for el in taple:
+#     print(el)
+
+## for loops with else
+# taple = (1,66,88,2,3,)
+# for el in taple:
+#     print(el)
+# else:
+#     print("end")
+
+# seq=range(0,6)
+# # print(seq[1])
+# # print(seq[2])
+# # print(seq[3])
+# # print(seq[4])
+# # print(seq[5])
+# for i in seq:
+#     print(i)
+
+# for i in range(5):
+#     print(i)
+
+# for i in range(0,11):
+#     print(i)
+
+# for i in range(0,11,2):
+#     print(i)
+
+## pass is a null statement that does nothing. 
+## It is used as a placeholder for future code.
+for i in range(0,11,2):
+    pass
 
